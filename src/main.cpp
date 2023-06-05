2ad8de72c69e96c95f6427ce9c7bf136511a86d5
d-mcadam <dylan@dylanmcadam.co.uk>
2023-06-05 18:35:05 +0100

    Added arguments for specifying the header file path and version number to increment

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
 #include "dylanclibs/log.h"
+#include "dylanclibs/cli_parser.h"
 
 LogSettings LOG_SETTINGS;
 
 int main(int argc, char* argv[]){
-    std::cout << "Testing line\n";
-    
-    //  TODO:   Need to
 
+    //  Will leave these values false always, as they will not be required for
+    //  the make, cmake or build file building the main executable of a given
+    //  project binary.
     LOG_SETTINGS.ls_use_working_dir = false;
     LOG_SETTINGS.ls_print_to_file = false;
     
@@ -20,6 +21,34 @@ int main(int argc, char* argv[]){
     } catch (std::exception &e) {
         std::cout << "Program is unable to start: " << e.what() << std::endl;
         exit(0);
+    }
+
+    ParserOption headerFilePath = ParserOption(
+        {"p", "header file path"},
+        "The location of the version number header file.",
+        true,
+        "path to header");
+
+    ParserOption versionNumber = ParserOption(
+        {"n", "version number"},
+        "Version number to increment.",
+        {"MAJOR", "MINOR", "PATCH", "BUILD"},
+        "BUILD");
+
+    Parser parser = Parser(argc, argv, "Automatically increments version numbers "
+            "in a header file for a project.");
+
+    parser.AddHelpOption();
+    parser.AddVersionOption();
+    int r = parser.AddOptions({headerFilePath, versionNumber});
+    if (r != 0){
+        elog << "Failed adding some options to the parser: " << r;
+    }
+
+    if (parser.Process()){
+
+    }else{
+        flog << "Failed to parse args.";
     }
 
     LogShutdown;
